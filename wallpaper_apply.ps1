137abb3f7db8b77a45bd78fb14358d8d380a878c
tianodont <104167316+Tianodont@users.noreply.github.com>
2025-08-26 16:09:46 +0300

    Update wallpaper_apply.ps1

--- wallpaper_apply.ps1
+++ wallpaper_apply.ps1
@@ -1,12 +1,14 @@
-$wallpaperPath = "C:\Users\Public\Pictures\wallpaper.jpg"
+$wallpaperPath = "C:\Windows\Web\Wallpaper\wallpaper.jpg"
 $url = "https://raw.githubusercontent.com/Tianodont/imghost/82e305df330d00c5dab85a9ffae555eb12a1bf08/98d1f355d65952c1c506021a8ab57832cd4cb39d.jpg"
 
 # Скачиваем обои
 curl $url -o $wallpaperPath
 
-# Применяем через ActiveDesktop COM
-$ad = New-Object -ComObject "Shell.Application"
-$desktop = $ad.Namespace(0).Self
-$desktopDesktop = New-Object -ComObject "Microsoft.ActiveDesktop"
-$desktopDesktop.SetWallpaper($wallpaperPath, 0)
-$desktopDesktop.ApplyChanges(0x2)  # AD_APPLY_FORCE
+# Обновляем реестр
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name Wallpaper -Value $wallpaperPath
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -Value 10
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -Value 0
+
+# Перезапускаем Explorer для применения
+Stop-Process -Name explorer -Force
+Start-Process explorer

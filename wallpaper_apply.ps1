046524c5c355bb6bb902ed06f9618d11275e40ae
tianodont <104167316+Tianodont@users.noreply.github.com>
2025-08-26 15:43:46 +0300

    Create wallpaper_apply.ps1

---
+++ wallpaper_apply.ps1
@@ -0,0 +1,29 @@
+# Путь к обоям
+$wallpaperPath = "$env:USERPROFILE\Pictures\wallpaper.jpg"
+
+# Создаём папку для обоев
+$dir = Split-Path $wallpaperPath
+if (-not (Test-Path $dir)) {
+    New-Item -Path $dir -ItemType Directory | Out-Null
+}
+
+# Ссылка на обои
+$url = 'https://raw.githubusercontent.com/Tianodont/imghost/82e305df330d00c5dab85a9ffae555eb12a1bf08/98d1f355d65952c1c506021a8ab57832cd4cb39d.jpg'
+
+# Скачиваем картинку
+Invoke-WebRequest -Uri $url -OutFile $wallpaperPath -Headers @{ "User-Agent" = "Mozilla/5.0" } -UseBasicParsing -TimeoutSec 30
+
+# Функция установки обоев
+function Set-Wallpaper {
+    Add-Type @"
+using System.Runtime.InteropServices;
+public class Wallpaper {
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern bool SystemParametersInfo(int uAction,int uParam,string lpvParam,int fuWinIni);
+}
+"@
+    [Wallpaper]::SystemParametersInfo(20, 0, $wallpaperPath, 3)
+}
+
+# Применяем обои сразу
+Set-Wallpaper
